731c1f37eae2dc576555605aa24b1a2c8e53574c
Simon Mutch <smutch@unimelb.edu.au>
2024-01-08 16:59:37 +1100

    Basic navbar

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Simon Mutch';
+export const SITE_DESCRIPTION = 'Homepage of Simon Mutch';
