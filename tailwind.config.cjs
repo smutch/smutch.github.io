9ffadd0e680c09158f91994ec57eff714c092a55
Simon Mutch <smutch@unimelb.edu.au>
2024-01-09 10:09:05 +1100

    :zap: Start working on basic index page design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,9 @@ module.exports = {
 			fontFamily: {
 				sans: ['Inter var', ...defaultTheme.fontFamily.sans],
 			},
+			height: {
+				custom: 'calc(100vh - var(--header-height))',
+			},
 		},
 	},
 	plugins: [],
